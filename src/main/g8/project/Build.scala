d646771b49c5432bdaab710b55707f8ee3c6691f
Michael Schleichardt <michael@schleichardt.net>
2013-05-25 09:37:56 +0200

    removed duplicate commas

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -4,9 +4,9 @@ import sbt.Keys._
 object Build extends Build {
 
   lazy val rootProject = Project(id = "$name;format="norm"$", base = file("."), settings = Project.defaultSettings ++ Seq(
-      name := "$name$",
-      , organization := "$organization$",
-      , version := "$version$",
+      name := "$name$"
+      , organization := "$organization$"
+      , version := "$version$"
       , scalaVersion := "$scala_version$"
     ) ++ seq(com.github.retronym.SbtOneJar.oneJarSettings: _*)
   )
